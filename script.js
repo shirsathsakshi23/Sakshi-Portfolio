1bc5990c2ad247b0a801ccf6dc9576bb5d782df1
shirsathsakshi23 <shirsathsakshi23@gmail.com>
2024-12-07 19:25:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Placeholder for any future JavaScript functionality. 
+// You can add interactivity if required later, like printing a downloadable CV or displaying hidden sections.
+
+console.log("CV Loaded successfully");
